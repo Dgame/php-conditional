d29a572af89bf8f228cececc6bdac35bcb67325c
Dgame <rswhite4@googlemail.com>
2017-01-23 08:04:41 +0100

    Test

--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -7,7 +7,6 @@ class VersionTest extends TestCase
 {
     public function testBelow()
     {
-        $this->assertTrue(version('7.0')->isBelow());
         $this->assertTrue(version('5.6')->isBelow());
         $this->assertTrue(version('7.0')->orBelow());
         $this->assertTrue(version('5.6')->orBelow());
@@ -16,10 +15,7 @@ class VersionTest extends TestCase
 
     public function testAbove()
     {
-        list($mayor, $minor, $patch) = explode('.', PHP_VERSION);
-
-        $this->assertTrue(version('7.0' . ($patch + 1))->isAbove());
-        $this->assertTrue(version('7.' . ($minor + 1))->isAbove());
+        $this->assertTrue(version('8.0.0')->isAbove());
         $this->assertTrue(version(PHP_VERSION)->orAbove());
     }
 
